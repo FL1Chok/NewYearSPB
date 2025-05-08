12e5e1896f4faef0cb134d267d7b0e1c316e369d
FL1Chok <ivanmironov06777@gmail.com>
2025-05-08 17:30:12 +0300

    Initial Commit

---
+++ NewYearSPB/NewYearSPBApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewYearSPBApp.swift
+//  NewYearSPB
+//
+//  Created by Иван Миронов on 08.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewYearSPBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewYearSPBTests/NewYearSPBTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewYearSPBTests.swift
+//  NewYearSPBTests
+//
+//  Created by Иван Миронов on 08.05.2025.
+//
+
+import Testing
+@testable import NewYearSPB
+
+struct NewYearSPBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
